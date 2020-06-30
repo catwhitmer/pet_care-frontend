c09120648330e550bbef1e0df4f99a920e0884a6
catwhitmer <catwhitmer@gmail.com>
2020-06-30 19:25:46 -0400

    added the ownersContainer to render OwnerForm and Owners

--- src/containers/OwnersContainer.js
+++ src/containers/OwnersContainer.js
@@ -1,4 +1,18 @@
-export default function ownerReducer(state = {owners: []}, action) {
+import React from 'react'
+import OwnerForm from '../components/OwnerForm'
+import Owners from '../components/Owners'
 
-  
+class OwnersContainer extends React.Component {
+    
+    render() {
+        return (
+            <div>
+                <OwnerForm />
+                <Owners />
+            </div>
+        )
+    }
+
 }
+
+export default OwnersContainer

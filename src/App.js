f5980243be29663f526fd03711ca02125821d454
catwhitmer <catwhitmer@gmail.com>
2020-06-30 19:26:33 -0400

    updated app.js to render OwnersContainer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import OwnersContainer from'./containers/OwnersContainer'
 
 class App extends React.Component {
   
   render(){
     return(
       <div className="App">
-        App
+        <OwnersContainer />
       </div>
     )
   }
 }
 
-export default App;
+export default connect()(App)

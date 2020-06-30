7426f8356a0c2743c4865aa051831a407cb38c0e
catwhitmer <catwhitmer@gmail.com>
2020-06-30 19:18:00 -0400

    added redux and react dependencies and added them to index.js, including redux devtools

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+import ownerReducer from './reducers/ownerReducer'
 
 import App from './App';
 
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(ownerReducer, composeEnhancers(applyMiddleware(thunk))) 
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={ store }>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
-);
-
+)

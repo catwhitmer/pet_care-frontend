70222c89073cfc2f651241c446891cd2c054ec31
catwhitmer <catwhitmer@gmail.com>
2020-06-30 20:16:23 -0400

    added the mapStateToProps function and passed props down to owners.js

--- src/containers/OwnersContainer.js
+++ src/containers/OwnersContainer.js
@@ -1,18 +1,31 @@
 import React from 'react'
+import { connect } from 'react-redux'
+
+import { fetchOwners } from '../actions/fetchOwners'
 import OwnerForm from '../components/OwnerForm'
 import Owners from '../components/Owners'
 
 class OwnersContainer extends React.Component {
+
+    componentDidMount() {
+        this.props.fetchOwners()
+    }
     
     render() {
         return (
             <div>
                 <OwnerForm />
-                <Owners />
+                <Owners owners={this.props.owners}/>
             </div>
         )
     }
 
 }
 
-export default OwnersContainer
+const mapStateToProps = state => {
+    return {
+        owners: state.owners
+    }
+}
+
+export default connect(mapStateToProps, { fetchOwners })(OwnersContainer)

0799db1af5c46c0a691ec7ea34c0115ce7b7a87a
catwhitmer <catwhitmer@gmail.com>
2020-06-30 20:14:50 -0400

    added fetch request to fetchOwners.js

--- src/actions/fetchOwners.js
+++ src/actions/fetchOwners.js
@@ -1,7 +1,10 @@
-export default function fetchOwners() {
-
-    //fetch('http://localhost:3001/api/v1/owners')
-    //.then(resp => resp.json())
-    //.then(data => console.log(data))
-
+export function fetchOwners() {
+   return (dispatch) => {
+    fetch('http://localhost:3001/api/v1/owners')
+    .then(resp => resp.json())
+    .then(owners => dispatch({
+        type: 'FETCH_OWNERS',
+        payload: owners
+    }))
+   }
 }

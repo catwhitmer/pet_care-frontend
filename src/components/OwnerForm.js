aa4f701dfe0d51fab868051872eb3702f878123c
catwhitmer <catwhitmer@gmail.com>
2020-07-01 13:33:43 -0400

    added state,a controlled form, and handle methods

--- src/components/OwnerForm.js
+++ src/components/OwnerForm.js
@@ -1,14 +1,39 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { addOwner } from '../actions/addOwner'
 
 class OwnerForm extends React.Component {
+
+    state = {
+        name: '',
+        email: ''
+    }
+
+    handleOnChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleOnSubmit = (event) => {
+        event.preventDefault()
+        this.props.addOwner(this.state)
+    }
 
     render() {
         return(
             <div>
-                OwnerForm
+                <form onSubmit={this.handleOnSubmit}>
+                    <label>Owner Name</label>
+                    <input type="text" placeholder='Name' value={this.state.name} name='name' onChange={this.handleOnChange} /><br></br>
+                    <label>Owner Email</label>
+                    <input type="text" placeholder='Email' value={this.state.email} name='email' onChange={this.handleOnChange} /><br></br>
+                    <input type='Submit' /> 
+                </form>
             </div>
         )
     }
 }
 
-export default OwnerForm
+
+export default connect(null, { addOwner })(OwnerForm)

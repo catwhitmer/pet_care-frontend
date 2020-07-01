cb0947af6f9e218b154dcb2dc17e7108e0ea8a1c
catwhitmer <catwhitmer@gmail.com>
2020-07-01 19:23:24 -0400

    added owner variable to help render owner component

--- src/components/Owner.js
+++ src/components/Owner.js
@@ -3,9 +3,13 @@ import React from 'react'
 
 const Owner = (props) => {
 
+console.log(props)
+
+let owner = props.owners[props.match.params.id - 1]
+
     return (
       <li>
-        {props.owner.name} - {props.owner.email}
+        {owner ? owner.name : null} - {owner ? owner.email : null}
       </li>
     )
 }

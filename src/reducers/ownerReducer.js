22dacd9aa956371480fdac3c8aa1fc41e5f8dd99
catwhitmer <catwhitmer@gmail.com>
2020-06-30 20:15:43 -0400

    added the switch statement to the ownerReducer for the fetch owner action type

--- src/reducers/ownerReducer.js
+++ src/reducers/ownerReducer.js
@@ -1,4 +1,10 @@
 export default function ownerReducer(state = {owners: []}, action) {
 
-  
+    switch (action.type){
+        case 'FETCH_OWNERS':
+        return {owners: action.payload}
+
+    default:
+        return state
+    }
 }

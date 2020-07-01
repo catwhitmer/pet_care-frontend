58442cbe4863d3117d1d7e420343372619422f13
catwhitmer <catwhitmer@gmail.com>
2020-07-01 18:10:21 -0400

    added router to index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
+import { BrowserRouter as Router } from 'react-router-dom'
 import ownerReducer from './reducers/ownerReducer'
 
 import App from './App';
@@ -15,7 +16,9 @@ const store = createStore(ownerReducer, composeEnhancers(applyMiddleware(thunk))
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={ store }>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')

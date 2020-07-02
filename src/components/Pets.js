ad11c213de19604e02ec03ee81e7bd0ce667bbea
catwhitmer <catwhitmer@gmail.com>
2020-07-02 17:14:30 -0400

    added a delete button and the handleDelete function

--- src/components/Pets.js
+++ src/components/Pets.js
@@ -1,15 +1,22 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { deletePet } from '../actions/deletePet'
 
 const Pets = (props) => {
+
+    const handleDelete = (pet) => {
+        props.deletePet(pet.id, pet.owner_id)
+    }
 
         return (
             <div>
             My Pets
                 {props.pets && props.pets.map(pet => 
-                    <li key={pet.id}>Name:{pet.name} - Kind:{pet.kind} - Age:{pet.age} - Breed:{pet.breed}</li>
+                    <li key={pet.id}>Name:{pet.name} - Kind:{pet.kind} - Age:{pet.age} - Breed:{pet.breed} 
+                    <button onClick={() => handleDelete(pet)}>Delete</button></li>
                 )}
             </div>
         )
 }
 
-export default Pets
+export default connect(null, { deletePet })(Pets)

73d3618513d27cb30c8bdca6fc8bc86993e0f49e
catwhitmer <catwhitmer@gmail.com>
2020-07-02 15:10:23 -0400

    added pet form

--- src/components/PetForm.js
+++ src/components/PetForm.js
@@ -1,20 +1,53 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
 
 class PetForm extends React.Component {
 
-    state= {
+    state = {
         name: '',
+        kind: '',
+        age: '',
+        breed: ''
 
     }
+
+    hanldeOnChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+    
+    handleOnSubmit = (event) => {
+        event.preventDefault()
+        this.props.addPet(this.state)
+        this.setState({
+            name: '',
+            kind: '',
+            age: '',
+            breed: ''
+        })
+    }
+
 
     render() {
         return (
             <div>
-                PetForm
+                <form onSubmit={this.handleOnSubmit}>
+                Add A New Pet here! <br></br>
+                    <label>Pet Name:</label>
+                    <input type='text' name='name' value={this.state.name} onChange={this.hanldeOnChange}/><br></br>
+                    <label>Pet Kind:</label>
+                    <input type='text' name='kind' value={this.state.kind} onChange={this.hanldeOnChange} /><br></br>
+                    <label>Pet Age:</label>
+                    <input type='text' name='age' value={this.state.age} onChange={this.hanldeOnChange} /><br></br>
+                    <label>Pet Breed:</label>
+                    <input type='text' name='breed' value={this.state.breed} onChange={this.hanldeOnChange} /><br></br>
+                    <input type='submit' />
+                </form>
             </div>
         )
     }
 }
 
-export default PetForm
+export default connect(null)(PetForm)

c8e314cd68728eedc61ea2065bf456f89005ffa1
catwhitmer <catwhitmer@gmail.com>
2020-07-02 15:52:40 -0400

    added some styling

--- src/components/Pets.js
+++ src/components/Pets.js
@@ -6,7 +6,7 @@ const Pets = (props) => {
             <div>
             My Pets
                 {props.pets && props.pets.map(pet => 
-                    <li key={pet.id}>{pet.name} - {pet.kind} - {pet.age} - {pet.breed}</li>
+                    <li key={pet.id}>Name:{pet.name} - Kind:{pet.kind} - Age:{pet.age} - Breed:{pet.breed}</li>
                 )}
             </div>
         )

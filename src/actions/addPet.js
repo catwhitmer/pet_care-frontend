e0b6a807053a5efe6dba12938ed5b56b95168911
catwhitmer <catwhitmer@gmail.com>
2020-07-02 15:53:24 -0400

    added fetch request

---
+++ src/actions/addPet.js
@@ -0,0 +1,14 @@
+export const addPet = (pet, ownerId) => {
+
+    return (dispatch) => {
+        fetch(`http://localhost:3001/api/v1/owners/${ownerId}/pets`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(pet)
+        })
+        .then(resp => resp.json())
+        .then(owner => dispatch({type: 'ADD_PET', payload: owner}))
+    }
+}

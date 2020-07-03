59a9e8e290f6c444b5eae912cbf94fe466131afd
catwhitmer <catwhitmer@gmail.com>
2020-07-03 17:37:54 -0400

    added basic layout for the todo form

---
+++ src/components/TodoForm.js
@@ -0,0 +1,14 @@
+import React from 'react' 
+
+class TodoForm extends React.Component {
+
+    render() {
+        return (
+            <div>
+                TodoForm
+            </div>
+        )
+    }
+}
+
+export default TodoForm

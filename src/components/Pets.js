1963eb0e19297fa75a7d3c4dc89f6a15fe5f0842
catwhitmer <catwhitmer@gmail.com>
2020-07-03 17:36:46 -0400

    added some styling

--- src/components/Pets.js
+++ src/components/Pets.js
@@ -9,15 +9,15 @@ const Pets = (props) => {
     const handleDelete = (pet) => {
         props.deletePet(pet.id, pet.owner_id)
     }
-console.log(props)
         return (
-            <h3>
-            <h2>My Pets</h2>
-                {props.pets && props.pets.map(pet => 
-                    <li key={pet.id}><Link to={`/owners/${props.owner.id}/pets/${pet.id}`}> {pet.name} </Link> - Kind: {pet.kind} - Age: {pet.age} - Breed: {pet.breed} 
-                    <button onClick={() => handleDelete(pet)}>Delete</button></li>
-                )}
-            </h3>
+            <h2>My Pets
+                <h3>
+                    {props.pets && props.pets.map(pet => 
+                        <li key={pet.id}><Link to={`/owners/${props.owner.id}/pets/${pet.id}`}> {pet.name} </Link> - Kind: {pet.kind} - Age: {pet.age} - Breed: {pet.breed} 
+                        <button onClick={() => handleDelete(pet)}>Delete</button></li>
+                    )}
+                </h3>
+            </h2>
         )
 }
 

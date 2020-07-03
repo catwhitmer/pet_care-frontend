987ab6a23837ea8c2534ac32845c5453220ea8a1
catwhitmer <catwhitmer@gmail.com>
2020-07-03 18:17:15 -0400

    finished the controlled todo form

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,14 +1,43 @@
 import React from 'react' 
+import { connect } from 'react-redux'
+import { addTodo } from '../actions/addTodo'
 
 class TodoForm extends React.Component {
+
+    state = {
+        description: '',
+        notes: ''
+    }
+
+    handleOnChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleOnSubmit = (event) => {
+        event.preventDefault()
+        this.props.addTodo(this.state, this.props.owner.id)
+        this.setState({
+            description: '',
+            notes: ''
+        })
+    }
 
     render() {
         return (
             <div>
-                TodoForm
+                <form onSubmit={this.handleOnSubmit}>
+                <h1>Add a New Todo</h1>
+                    <label>description</label>
+                    <input type='text' name='description' value={this.state.description} onChange={this.hanldeOnChange} /><br />
+                    <label>notes</label>
+                    <input type='text' name='notes' value={this.state.notes} onChange={this.hanldeOnChange} /><br />
+                    <input type='submit'/>
+                </form>
             </div>
         )
     }
 }
 
-export default TodoForm
+export default connect(null, { addTodo }) (TodoForm)

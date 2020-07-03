fcf4604fe35cc3e4125968f42e99a40aa6b5896a
catwhitmer <catwhitmer@gmail.com>
2020-07-03 17:36:00 -0400

    lined up the li elements

--- src/components/Owners.js
+++ src/components/Owners.js
@@ -4,12 +4,11 @@ import Owner from './Owner'
 
 const Owners = (props) => {
 
-console.log(props)
     return (
         <h3 className='owners'>
         <h1>Click your name to Sign In!</h1>
             {props.owners.map(owner => 
-               <li key={owner.id}>
+                <li key={owner.id}>
                     <Link to={`/owners/${owner.id}`}> {owner.name} </Link> - {owner.email}
                 </li>)}
         </h3>

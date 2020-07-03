c642e59638a0eb15ad70dba059152a4410f9a12a
catwhitmer <catwhitmer@gmail.com>
2020-07-03 14:07:21 -0400

    added some styling

--- src/components/Owners.js
+++ src/components/Owners.js
@@ -6,13 +6,13 @@ const Owners = (props) => {
 
 console.log(props)
     return (
-        <div>
-        Click your name to Sign In 
+        <h3 className='owners'>
+        <h1>Click your name to Sign In!</h1>
             {props.owners.map(owner => 
-                <li key={owner.id}>
+               <li key={owner.id}>
                     <Link to={`/owners/${owner.id}`}> {owner.name} </Link> - {owner.email}
                 </li>)}
-        </div>
+        </h3>
 
     )
 }

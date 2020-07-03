9a5819d666943bd39d2e57c17764504262bc0ffd
catwhitmer <catwhitmer@gmail.com>
2020-07-03 18:16:11 -0400

    added fetch request

---
+++ src/actions/addTodo.js
@@ -0,0 +1,14 @@
+export const addTodo = (todo, ownerId, petId) => {
+
+    return (dispatch) => {
+        fetch(`http://localhost:3001/api/v1/owners/${ownerId}/pets/${petId}/todos`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(todo)
+        })
+        .then(resp => resp.json())
+        .then(owner => dispatch({type: 'ADD_TODO', payload: owner}))
+    }
+}

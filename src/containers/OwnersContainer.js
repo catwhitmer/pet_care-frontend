de83b2523a655ec68cbe8881e9df3101c916a768
catwhitmer <catwhitmer@gmail.com>
2020-07-04 15:43:24 -0400

    imported home page and added a route

--- src/containers/OwnersContainer.js
+++ src/containers/OwnersContainer.js
@@ -8,6 +8,7 @@ import PetForm from '../components/PetForm'
 import Owners from '../components/Owners'
 import Owner from '../components/Owner'
 import Pets from '../components/Pets'
+import Home from '../components/Home'
 import RenderPetForm from '../components/RenderPetForm'
 import NavBar from '../components/NavBar'
 
@@ -22,10 +23,12 @@ class OwnersContainer extends React.Component {
             <div>
             <NavBar />
                 <Switch>
-                    <Route path='/owners/new' component={OwnerForm} />
-                    <Route path='/owners/:id/pets/new' component={PetForm} />
                     <Route path='/owners/:id' render={(routerProps) => <Owner {...routerProps} owners={this.props.owners}/>} />
                     <Route path='/owners' render= {(routerProps) => <Owners {...routerProps} owners={this.props.owners}/>} />
+                    <Route path='/owners/:id/pets/' render= {(routerProps) => <Owners {...routerProps} owners={this.props.owners}/>} />
+                    <Route path='/owners/new' component={OwnerForm} />
+                    <Route path='/owners/:id/pets/new' component={PetForm} />
+                    <Route path='/' component={Home} />
                 </Switch>
             </div>
         )

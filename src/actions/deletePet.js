a7e4e2964fd840b208e2d56659992b1a47ee4bf5
catwhitmer <catwhitmer@gmail.com>
2020-07-04 16:04:06 -0400

    fixed spelling errors

--- src/actions/deletePet.js
+++ src/actions/deletePet.js
@@ -1,12 +1,10 @@
-export const deletePet = (petId, ownerId) => {
+export const deletePet = (ownerId, petId) => {
 
     return (dispatch) => {
-        return fetch(`http://localhost:3001/api/v1/owners/${ownerId}/pets${petId}`, {
+        return fetch(`http://localhost:3001/api/v1/owners/${ownerId}/pets/${petId}`, {
             method: 'DELETE'
         })
         .then(resp => resp.json())
-        .then(owner = dispatch({type: 'DELETE_PET', payload: owner}))
+        .then(owner => dispatch({type: 'DELETE_PET', payload: owner}))
     }
 }
-
-export default deletePet

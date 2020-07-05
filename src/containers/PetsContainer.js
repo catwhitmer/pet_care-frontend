d5db2640a20aa9b1151156248ba3e3c8ad024999
catwhitmer <catwhitmer@gmail.com>
2020-07-05 16:31:19 -0400

    added todos component back in to check add features

--- src/containers/PetsContainer.js
+++ src/containers/PetsContainer.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PetForm from '../components/PetForm'
 import Pets from '../components/Pets'
 import Pet from '../components/Pet'
+import TodosContainer from '../containers/TodosContainer'
 
 
 class PetsContainer extends React.Component {
@@ -11,6 +12,7 @@ class PetsContainer extends React.Component {
             <div>
                 <Pets pets={this.props.owner && this.props.owner.pets} owner={this.props.owner}/>
                 <PetForm owner={this.props.owner} />
+                <TodosContainer  owner={this.props.owner}/>
             </div>
         )
     }

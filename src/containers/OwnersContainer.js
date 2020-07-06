c1a157bff29b18b73d0a5d87f8cff57c52911021
catwhitmer <catwhitmer@gmail.com>
2020-07-05 20:26:01 -0400

    added withRouter, added a todos route, and imported Todo

--- src/containers/OwnersContainer.js
+++ src/containers/OwnersContainer.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, withRouter } from 'react-router-dom'
 
 import { fetchOwners } from '../actions/fetchOwners'
 import OwnerForm from '../components/OwnerForm'
 import PetForm from '../components/PetForm'
 import Owners from '../components/Owners'
 import Owner from '../components/Owner'
-import pets from '../components/Pets'
-import Pet from '../components/Pet'
+import Pets from '../components/Pets'
+import Todo from '../components/Todo'
 import Home from '../components/Home'
 import NavBar from '../components/NavBar'
 
@@ -17,18 +17,19 @@ class OwnersContainer extends React.Component {
     componentDidMount() {
         this.props.fetchOwners()
     }
-    
+  
     render() {
         return (
             <div>
             <NavBar />
                 <Switch>
-                    <Route path='/owners/new' component={OwnerForm} />
-                    <Route path='/owners/:id/pets/new' component={PetForm} />
-                    <Route path='/owners/:id' render={(props) => <Owner {...props} owners={this.props.owners}/>} />
-                    <Route path='/owners' render= {(props) => <Owners {...props} owners={this.props.owners}/>} />
-                    <Route path='/owners/:id/pets/:id' render= {(props) => <Owners {...props} owners={this.props.owners}/>} /> 
-                    <Route path='/' component={Home} />
+                    <Route exact path='/owners/new' component={OwnerForm} />
+                    <Route exact path='/owners/:id/pets/new' component={PetForm} />
+                    <Route exact path='/owners/:id/pets/:id/todos' render={(props) => <Todo {...props} owners={this.props.owners}/>} />
+                    <Route exact path='/owners/:id' render={(props) => <Owner {...props} owners={this.props.owners}/>} />
+                    <Route exact path='/owners' render= {(props) => <Owners {...props} owners={this.props.owners}/>} />
+                    <Route exact path='/owners/:id/pets/:id' render= {(props) => <Owners {...props} owners={this.props.owners}/>} />             
+                    <Route exact path='/' component={Home} />
                 </Switch>
             </div>
         )
@@ -42,4 +43,4 @@ const mapStateToProps = state => {
     }
 }
 
-export default connect(mapStateToProps, { fetchOwners })(OwnersContainer)
+export default withRouter (connect(mapStateToProps, { fetchOwners })(OwnersContainer))

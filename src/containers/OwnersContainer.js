45d03352f04fa39b30cda914b05a74c6c388c842
catwhitmer <catwhitmer@gmail.com>
2020-07-06 19:40:49 -0400

    imported Todo form

--- src/containers/OwnersContainer.js
+++ src/containers/OwnersContainer.js
@@ -5,6 +5,7 @@ import { Route, Switch, withRouter } from 'react-router-dom'
 import { fetchOwners } from '../actions/fetchOwners'
 import OwnerForm from '../components/OwnerForm'
 import PetForm from '../components/PetForm'
+import TodoForm from '../components/TodoForm'
 import Owners from '../components/Owners'
 import Owner from '../components/Owner'
 import Pets from '../components/Pets'
@@ -25,6 +26,7 @@ class OwnersContainer extends React.Component {
                 <Switch>
                     <Route exact path='/owners/new' component={OwnerForm} />
                     <Route exact path='/owners/:id/pets/new' component={PetForm} />
+                    <Route exact path='/owners/:owner_id/pets/:id/todos/new' component={TodoForm} />
                     <Route exact path='/owners/:owner_id/pets/:id/todos' render={(props) => <Todos {...props} owners={this.props.owners}/>}/>
                     <Route exact path='/owners/:id' render={(props) => <Owner {...props} owners={this.props.owners}/>} />
                     <Route exact path='/owners' render= {(props) => <Owners {...props} owners={this.props.owners}/>} />      

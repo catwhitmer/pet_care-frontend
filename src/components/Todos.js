18509287661e0fdc082e12610fee4253a7605e06
catwhitmer <catwhitmer@gmail.com>
2020-07-06 16:12:39 -0400

    added todos iteration

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -3,10 +3,12 @@ import TodosContainer from '../containers/TodosContainer'
 
 
 const Todos = (props) => {
-
+console.log(props)
     return (
         <div>
             <h1>To-Dos</h1>
+             {/*props.todos.map(todo => 
+                    <div key={todo.id}> {todo.description} - {todo.notes} </div>)*/}
         </div>
     )
 }

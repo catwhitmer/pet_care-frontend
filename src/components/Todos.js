4855f01152e289d9b26203914602e7a6f0e24360
catwhitmer <catwhitmer@gmail.com>
2020-07-06 12:58:18 -0400

    removed not needed import

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,17 +1,13 @@
 import React from 'react'
-import TodoForm from './TodoForm'
-import Todo from './Todo'
+import TodosContainer from '../containers/TodosContainer'
+
 
 const Todos = (props) => {
 
     return (
         <div>
             <h1>To-Dos</h1>
-                <h3>
-                    {props.todos && props.todos.map(todo =>
-                        <li key={todo.id}>{todo.description} - {todo.notes}</li>
-                    )}
-                </h3>
+          
         </div>
     )
 }

c59c176fa189bdf2db0e4ca295e3a70a0a471fd7
catwhitmer <catwhitmer@gmail.com>
2020-07-05 20:26:26 -0400

    imported Todo

--- src/containers/PetsContainer.js
+++ src/containers/PetsContainer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PetForm from '../components/PetForm'
 import Pets from '../components/Pets'
-import Pet from '../components/Pet'
+import Todo from '../components/Todo'
 
 
 class PetsContainer extends React.Component {
@@ -10,7 +10,7 @@ class PetsContainer extends React.Component {
         return (
             <div>
                 <Pets pets={this.props.owner && this.props.owner.pets} owner={this.props.owner}/>
-                <PetForm owner={this.props.owner} />            
+                <PetForm owner={this.props.owner} />  
             </div>
         )
     }

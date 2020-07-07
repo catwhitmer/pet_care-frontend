f8122f5289ee7ca7e859367cbf4afbd596c57e27
catwhitmer <catwhitmer@gmail.com>
2020-07-07 15:05:53 -0400

    iterated through pets and filtered todos to find todo and added edit and delete buttons

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,18 +1,28 @@
 import React from 'react'
 import { connect} from 'react-redux'
 
+import TodoForm from '../components/TodoForm'
+
 
 const Todos = (props) => {
 
+    const findTodos = () => {
+       return (
+        props.pets.find(pet => pet.id == props.match.params.id).todos.filter(todo => todo.pet_id == props.match.params.id)
+       )
+    }
+
     return (
         <div>
-        <h1>To-Dos</h1>
-            {props.todos && props.todos.map(todo => 
-                <div>
-                    {todo.description}{todo.notes}
-                </div>
-         
-            )}
+            <h1>To-Dos</h1>
+                <h3>
+                {props.pets.length  > 0 ? findTodos().map(todo => 
+                <li key={todo.id}> {todo.description} - {todo.notes} </li>) : null}
+                    <button onClick={() => this.handleEdit(pet)}>Edit</button>
+                    <button onClick={() => this.handleDelete(pet)}>Delete</button>
+                </h3>
+                <hr />
+                <TodoForm pets={props.pets}/>
         </div>
     )
 }
